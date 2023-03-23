074d7649a06f5598b7c724781832d1ea2745e2e3
4MJ_Cho <mjycho59@gmail.com>
2023-03-23 10:36:31 +0530

    environment set up

---
+++ src/app.module.ts
@@ -0,0 +1,19 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppService } from './app.service';
+import { DataSource } from 'typeorm';
+import { configService } from './config/config.service';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot(
+      configService.getTypeOrmConfig()
+    ),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {
+  private dataSource: DataSource
+}
