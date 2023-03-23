e2b16c7feac26196ca02b7cb564b041968ad0b9a
4MJ_Cho <mjycho59@gmail.com>
2023-03-23 11:44:50 +0530

    implimenting authentication with passport

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,16 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppService } from './app.service';
 import { DataSource } from 'typeorm';
 import { configService } from './config/config.service';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(
       configService.getTypeOrmConfig()
     ),
+    AuthModule,
+    UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
