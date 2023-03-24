d165d635ccc80c54fe2b23416ac06b8c2382b22b
4MJ_Cho <mjycho59@gmail.com>
2023-03-24 18:30:06 +0530

    Auth endpoints

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,35 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, UseGuards, Request, Body } from '@nestjs/common';
 import { AppService } from './app.service';
+import { AuthService } from './auth/auth.service';
+import { JwtAuthGuard } from './auth/guards/jwt-auth.guard';
+import { LocalAuthGuard } from './auth/guards/local-auth.guard';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private authService: AuthService
+    ) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Post('login')
+  async login(@Body() req) {
+    console.log(req);
+    return this.authService.login(req);
+  }
+
+  @Post('signup')
+  async sign_up(@Body() req){
+    return this.authService.sign_up(req);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('profile')
+  getProfile(@Request() req) {
+    return req.user;
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,16 +1,63 @@
 import { Injectable } from '@nestjs/common';
 import { UsersService } from 'src/users/users.service';
+import { JwtService } from '@nestjs/jwt';
+import { AuthUtils } from 'src/utils/auth.util';
+
 
 @Injectable()
 export class AuthService {
-    constructor(private usersService: UsersService) {}
-
+    constructor(
+      private usersService: UsersService,
+      private jwtService: JwtService
+      ) {}
+    //Validate a user
     async validateUser(email: string, pass: string): Promise<any> {
         const user = await this.usersService.findOne(email);
-        if (user && user.password === pass) {
-          const { password, ...result } = user;
-          return result;
+        if (user) {
+          const checkPassport = AuthUtils.comparePass(pass, user.password);
+          if (checkPassport) {
+            return user;
+          }
+          return {
+            message: 'Wrong Password'
+          };
         }
         return null;
+    }
+
+    //login
+    async login(user: any) {
+      const loginUser = await this.validateUser(user.email, user.password);
+      if(loginUser && !loginUser.message){
+        const payload = { email: user.email, pass: user.password };
+        return {
+          statusCode: 200,
+          message: 'Login Sucessfull',
+          access_token: this.jwtService.sign(payload),
+          user
+        };
+      }else if (loginUser && loginUser.message) {
+        return{
+          statusCode: 401,
+          message: 'Wrong Password',
+        }
+      }
+      return{
+          statusCode: 401,
+          message: 'User does not exist',
+      }
+    }
+
+    //sign up a user
+    async sign_up(user: any){
+      const checkUser = await this.usersService.findOne(user.email);
+      if(checkUser){
+        return {
+          message : 'User already exist, use another email',
+        }
+      };
+
+      const signup_user = await this.usersService.createUser(user);
+      return signup_user;
     }
 }
